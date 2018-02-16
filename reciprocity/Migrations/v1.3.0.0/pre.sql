ab251c572cc6a0c32f6e737e34951284276d5d6b
Patrick Paskaris <patrick@paskaris.gr>
2018-02-15 23:26:14 -0500

    Increment version to v1.3.0.0

---
+++ reciprocity/Migrations/v1.3.0.0/pre.sql
@@ -0,0 +1,16 @@
+﻿CREATE INDEX CNF_FoodName_FoodDescription
+	ON reciprocity.CNF_FoodName (FoodDescription);
+
+CREATE TABLE reciprocity.CNF_Unit (
+	MeasureId INT NOT NULL,
+	Serving DECIMAL(7,2) NOT NULL,
+	ServingType CHAR(1) NOT NULL,
+	ServingCode VARCHAR(3) NOT NULL,
+	Parenthetical NVARCHAR(127) NULL,
+
+	INDEX CNF_Unit_MeasureId (MeasureId),
+
+	CONSTRAINT Unit_CorrespondsTo_CNF_Unit
+		FOREIGN KEY (ServingType, ServingCode)
+		REFERENCES reciprocity.Unit (UnitTypeCode, UnitCode),
+);
